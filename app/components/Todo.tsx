aabf637138700cce83a49bbd8f6546c265e4eb2c
konapeso <n2kgt1@gmail.com>
2024-02-25 17:16:34 +0900

    タイトル編集機能追加

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -1,10 +1,46 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { TodoType } from "../types";
+import useSWR from "swr";
+
+async function fetcher(key: string) {
+  return fetch(key).then((res) => res.json());
+}
 
 type TodoProps = {
   todo: TodoType;
 };
 const Todo = ({ todo }: TodoProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todo.title);
+  const { data, isLoading, error, mutate } = useSWR(
+    "http://localhost:8080/allTodos",
+    fetcher
+  );
+
+  const handleEdit = async () => {
+    setIsEditing(!isEditing);
+    if (isEditing) {
+      const response = await fetch(
+        `http://localhost:8080/editTodo/${todo.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: editedTitle,
+          }),
+        }
+      );
+      if (response.ok) {
+        const editedTodo = await response.json();
+        mutate([...data, editedTodo]);
+        setEditedTitle("");
+      }
+    }
+  }; // Add closing curly brace here
+
   return (
     <div>
       <li className="py-4">
@@ -15,15 +51,27 @@ const Todo = ({ todo }: TodoProps) => {
               name="todo1"
               type="checkbox"
               className="h-4 w-4 text-teal-600 focus:ring-teal-500
-                  border-gray-300 rounded"
+                                    border-gray-300 rounded"
             />
             <label className="ml-3 block text-gray-900">
-              <span className="text-lg font-medium mr-2"> {todo.title}</span>
+              {isEditing ? (
+                <input
+                  type="text"
+                  className="border rounded py-1 px-2"
+                  value={editedTitle}
+                  onChange={(e) => setEditedTitle(e.target.value)}
+                />
+              ) : (
+                <span className="text-lg font-medium mr-2"> {todo.title}</span>
+              )}
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <button className="duration-150 bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 rounded">
-              ✒
+            <button
+              onClick={handleEdit}
+              className="duration-150 bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 rounded"
+            >
+              {isEditing ? "Save" : "✒"}
             </button>
             <button className="bg-red-500 hover:bg-red-600 text-white font-medium py-1 px-2 rounded">
               ✖
