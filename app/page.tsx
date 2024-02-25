625712e7bda807e07493c76e4d6c93dbf6d525ef
konapeso <n2kgt1@gmail.com>
2024-02-25 16:57:14 +0900

    キャッシュ取得

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ async function fetcher(key: string) {
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const { data, isLoading, error } = useSWR(
+  const { data, isLoading, error, mutate } = useSWR(
     "http://localhost:8080/allTodos",
     fetcher
   );
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const response = fetch("http://localhost:8080/createTodo", {
+    const response = await fetch("http://localhost:8080/createTodo", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -28,6 +28,14 @@ export default function Home() {
         isCompleted: false,
       }),
     });
+
+    if (response.ok) {
+      const newTodo = await response.json();
+      mutate([...data, newTodo]);
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+    }
   };
   return (
     <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-32 py-4 px-4">
